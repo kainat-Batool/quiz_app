bab3a9416ff3916d3972489ac8a06a918c7c178e
kainat-Siddique <91836914+kainat-Batool@users.noreply.github.com>
2023-06-19 09:48:19 +0500

    Adding more qustions

--- lib/main.dart
+++ lib/main.dart
@@ -44,19 +44,172 @@ class _MyAppState extends State<MyApp> {
         {'text': 'Max', 'score': 2},
         {'text': 'Max', 'score': 5}
       ],
-    }
+    },
+    {
+      'questionText': 'What is your favorite food?',
+      'answers': [
+        {'text': 'Pizza', 'score': 10},
+        {'text': 'Sushi', 'score': 8},
+        {'text': 'Burger', 'score': 6},
+        {'text': 'Pasta', 'score': 7}
+      ]
+    },
+    {
+      'questionText': 'Which is your favorite season?',
+      'answers': [
+        {'text': 'Spring', 'score': 8},
+        {'text': 'Summer', 'score': 10},
+        {'text': 'Autumn', 'score': 6},
+        {'text': 'Winter', 'score': 7}
+      ]
+    },
+    {
+      'questionText': 'What is your favorite book genre?',
+      'answers': [
+        {'text': 'Mystery', 'score': 6},
+        {'text': 'Fantasy', 'score': 8},
+        {'text': 'Science Fiction', 'score': 10},
+        {'text': 'Romance', 'score': 5}
+      ]
+    },
+    {
+      'questionText': 'Who is your favorite superhero?',
+      'answers': [
+        {'text': 'Iron Man', 'score': 8},
+        {'text': 'Spider-Man', 'score': 10},
+        {'text': 'Batman', 'score': 7},
+        {'text': 'Superman', 'score': 6}
+      ]
+    },
+    {
+      'questionText': 'What is your favorite hobby?',
+      'answers': [
+        {'text': 'Reading', 'score': 8},
+        {'text': 'Sports', 'score': 10},
+        {'text': 'Painting', 'score': 6},
+        {'text': 'Cooking', 'score': 7}
+      ]
+    },
+    {
+      'questionText': 'What is your favorite movie genre?',
+      'answers': [
+        {'text': 'Action', 'score': 8},
+        {'text': 'Comedy', 'score': 9},
+        {'text': 'Drama', 'score': 7},
+        {'text': 'Thriller', 'score': 6}
+      ]
+    },
+    {
+      'questionText': 'Who is your favorite musician?',
+      'answers': [
+        {'text': 'Taylor Swift', 'score': 9},
+        {'text': 'Ed Sheeran', 'score': 8},
+        {'text': 'Beyoncé', 'score': 10},
+        {'text': 'Drake', 'score': 7}
+      ]
+    },
+    {
+      'questionText': 'What is your favorite outdoor activity?',
+      'answers': [
+        {'text': 'Hiking', 'score': 10},
+        {'text': 'Cycling', 'score': 8},
+        {'text': 'Swimming', 'score': 7},
+        {'text': 'Picnicking', 'score': 6}
+      ]
+    },
+    {
+      'questionText': 'Which is your favorite holiday destination?',
+      'answers': [
+        {'text': 'Beach resort', 'score': 9},
+        {'text': 'Mountain retreat', 'score': 10},
+        {'text': 'City exploration', 'score': 8},
+        {'text': 'Countryside getaway', 'score': 7}
+      ]
+    },
+    {
+      'questionText': 'What is your favorite sport?',
+      'answers': [
+        {'text': 'Football', 'score': 10},
+        {'text': 'Basketball', 'score': 9},
+        {'text': 'Tennis', 'score': 8},
+        {'text': 'Golf', 'score': 7}
+      ]
+    },
+    {
+      'questionText': 'What is your favorite drink?',
+      'answers': [
+        {'text': 'Coffee', 'score': 10},
+        {'text': 'Tea', 'score': 9},
+        {'text': 'Juice', 'score': 8},
+        {'text': 'Soda', 'score': 7}
+      ]
+    },
+    {
+      'questionText': 'What is your favorite ice cream flavor?',
+      'answers': [
+        {'text': 'Chocolate', 'score': 10},
+        {'text': 'Vanilla', 'score': 9},
+        {'text': 'Strawberry', 'score': 8},
+        {'text': 'Mint', 'score': 7}
+      ]
+    },
+    {
+      'questionText': 'What is your favorite social media platform?',
+      'answers': [
+        {'text': 'Facebook', 'score': 10},
+        {'text': 'Instagram', 'score': 9},
+        {'text': 'Twitter', 'score': 8},
+        {'text': 'Snapchat', 'score': 7}
+      ]
+    },
+    {
+      'questionText': 'What is your favorite subject in school?',
+      'answers': [
+        {'text': 'Math', 'score': 10},
+        {'text': 'Science', 'score': 9},
+        {'text': 'English', 'score': 8},
+        {'text': 'History', 'score': 7}
+      ]
+    },
+    {
+      'questionText': 'What is your favorite clothing item?',
+      'answers': [
+        {'text': 'Jeans', 'score': 10},
+        {'text': 'T-shirt', 'score': 9},
+        {'text': 'Sweater', 'score': 8},
+        {'text': 'Dress', 'score': 7}
+      ]
+    },
+    {
+      'questionText': 'What is your favorite season?',
+      'answers': [
+        {'text': 'Spring', 'score': 10},
+        {'text': 'Summer', 'score': 9},
+        {'text': 'Autumn', 'score': 8},
+        {'text': 'Winter', 'score': 7}
+      ]
+    },
+    {
+      'questionText': 'What is your favorite flower?',
+      'answers': [
+        {'text': 'Rose', 'score': 10},
+        {'text': 'Lily', 'score': 9},
+        {'text': 'Sunflower', 'score': 8},
+        {'text': 'Tulip', 'score': 7},
+      ]
+    },
   ];
   var questionIndex = 0;
   var _totalScore = 0;
 
-  void resetQuiz(){
+  void resetQuiz() {
     setState(() {
       questionIndex = 0;
       _totalScore = 0;
     });
   }
-  void answerQuestion(int score) {
 
+  void answerQuestion(int score) {
     _totalScore += score;
     setState(() {
       questionIndex = questionIndex + 1;
